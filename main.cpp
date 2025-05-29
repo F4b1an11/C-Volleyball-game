34c1aad6400ec3de2934e90777cf87027c937fb2
Fabian <ochoa.fabian15@gmail.com>
2025-05-29 15:24:16 -0500

    created and added controls for player two, and set origin point for both players

--- main.cpp
+++ main.cpp
@@ -4,14 +4,21 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({2048, 1024}), "SFML works! | Regina was here :D",sf::Style::Default);
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::White);
-    shape.setPosition(sf::Vector2(0.f, 0.f));
+    u_int32_t width = 2048U;
+    u_int32_t height = 1024U;
+
+    sf::RenderWindow window(sf::VideoMode({width, height}), "SFML works! | Regina was here :D",sf::Style::Default);
+
     sf::RectangleShape player1({69.f,100.0f});
     player1.setFillColor(sf::Color::Blue);
-    player1.setPosition(player1.getGeometricCenter());
+    player1.setOrigin(player1.getGeometricCenter());
+    player1.setPosition({static_cast<float>(width)/4, static_cast<float>(height)- player1.getSize().y/2});
+    sf::RectangleShape player2({69.f,100.0f});
+    player2.setFillColor(sf::Color::Red);
+    player2.setOrigin(player2.getGeometricCenter());
+    player2.setPosition({static_cast<float>(width)*3/4, static_cast<float>(height)- player2.getSize().y/2});
     window.setFramerateLimit(60);
+    window.setMinimumSize(sf::Vector2u(200u,200u));
     while (window.isOpen())
     {
         
@@ -27,8 +34,10 @@ int main()
                 sf::Vector2f left = {-10.0f,0.0f};
                 sf::Vector2f up = {0.0f,-10.0f};
                 sf::Vector2f down = {0.0f,10.0f};
+
                 if(keypressed->scancode == sf::Keyboard::Scancode::Escape){
                     window.close();
+                    std::cout << "Closeing window \n";
                 }
                 else if(keypressed->scancode == sf::Keyboard::Scancode::Right){
                     player1.move(right);
@@ -42,10 +51,24 @@ int main()
                 else if(keypressed->scancode == sf::Keyboard::Scancode::Down){
                     player1.move(down);
                 }
+                else if(keypressed->scancode == sf::Keyboard::Scancode::D){
+                    player2.move(right);
+                }
+                else if(keypressed->scancode == sf::Keyboard::Scancode::A){
+                    player2.move(left);
+                }
+                else if(keypressed->scancode == sf::Keyboard::Scancode::W){
+                    player2.move(up);
+                }
+                else if(keypressed->scancode == sf::Keyboard::Scancode::S){
+                    player2.move(down);
+                }
             }
             else if (event->is<sf::Event::Resized>()){
                 sf::Vector2u size = window.getSize();
-                std::cout << "width = " << size.x << " height = " << size.y << "\n" ;
+                width = size.x;
+                height = size.y;
+                std::cout << "width = " << width << " height = " << height << "\n" ;
             }
             else if (event->is<sf::Event::MouseButtonPressed>()){
                 std::cout << "Mouse Click" << std::endl;
@@ -54,7 +77,7 @@ int main()
         
 
         window.clear();
-        window.draw(shape);
+        window.draw(player2);
         window.draw(player1);
         window.display();
     }
