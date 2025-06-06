51c252d1a0d66f863a33a3a2f4f64f28c94faa10
Fabian <ochoa.fabian15@gmail.com>
2025-06-06 16:24:16 -0500

    created more bounds as well as added the ball

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 
 int main()
 {
-    u_int32_t width = 2048U;
-    u_int32_t height = 1024U;
+    u_int32_t width = 1420U;
+    u_int32_t height = 720U;
 
     sf::RenderWindow window(sf::VideoMode({width, height}), "SFML works! | Regina was here :D",sf::Style::Default);
     
@@ -17,10 +17,25 @@ int main()
     bound.setFillColor( sf::Color(0xFD5E53FF));
     Input input = {};
 
-    sf::RectangleShape bottomBound({static_cast<float>(width), 50.0f});
+    sf::RectangleShape bottomBound({static_cast<float>(width), 100.0f});
     bottomBound.setOrigin(bottomBound.getGeometricCenter());
     bottomBound.setFillColor(sf::Color(0xDFDFDFFF));
     bottomBound.setPosition({static_cast<float>(width)/2.0f,static_cast<float>(height)-50.0f});
+
+    sf::RectangleShape topBound({static_cast<float>(width), 100.0f});
+    topBound.setOrigin(topBound.getGeometricCenter());
+    topBound.setFillColor(sf::Color(0xDFDFDFFF));
+    topBound.setPosition({static_cast<float>(width)/2.0f,50.0f});
+
+    sf::RectangleShape leftBound({100.0f,static_cast<float>(width) });
+    leftBound.setOrigin(leftBound.getGeometricCenter());
+    leftBound.setFillColor(sf::Color(0xDFDFDFFF));
+    leftBound.setPosition({50,static_cast<float>(height)/2});
+
+    sf::RectangleShape rightBound({100.0f, static_cast<float>(width)});
+    rightBound.setOrigin(rightBound.getGeometricCenter());
+    rightBound.setFillColor(sf::Color(0xDFDFDFFF));
+    rightBound.setPosition({static_cast<float>(width)-50.0f,static_cast<float>(height)/2});
 
     sf::RectangleShape player1({69.f,100.0f});
     player1.setFillColor(sf::Color::Blue);
@@ -31,9 +46,16 @@ int main()
     player2.setFillColor(sf::Color::Red);
     player2.setOrigin(player2.getGeometricCenter());
     player2.setPosition({static_cast<float>(width)*3/4, static_cast<float>(height)- player2.getSize().y/2});
+
+    sf::RectangleShape ball({20.0f,20.0f});
+    ball.setFillColor(sf::Color(0xFFFFFFFF));
+    ball.setOrigin(ball.getGeometricCenter());
+    ball.setPosition({static_cast<float>(width)/2,static_cast<float>(height)*3/4});
+
+
     
     window.setFramerateLimit(120);
-    window.setMinimumSize(sf::Vector2u(200u,200u));
+    window.setMinimumSize(sf::Vector2u(200U,200U));
 
     
     const sf::Keyboard::Scancode ButtonKeys[BUTTON_COUNT] = {
@@ -45,6 +67,10 @@ int main()
 
     sf::Vector2f velocity = {0.0f,0.0f};
     sf::Vector2f acceleration = {0.0f, 0.0f};
+
+    sf::Vector2f ballVelocity = {0.0f,0.0f};
+    sf::Vector2f ballAcceleration = {0.0f, 0.0f};
+
     sf::Clock timeDelta;
     const float ACC = 0.01f;
     const float fr = 0.95f;
@@ -102,6 +128,8 @@ int main()
         
 
         acceleration = {0.0f,0.0f};
+        ballAcceleration = {0.0f,0.0f};
+
 
         if(input.buttons[BUTTON_UP].is_down && ((bottomBound.getPosition().y - bottomBound.getSize().y/2)-(player1.getPosition().y + player1.getSize().y/2) < 1)){
             acceleration.y  = -ACC*75;
@@ -115,7 +143,8 @@ int main()
         else if (input.buttons[BUTTON_RIGHT].is_down){
             acceleration.x = ACC;
         }
-        std:: cout << (bottomBound.getPosition().y - bottomBound.getSize().y/2)-(player1.getPosition().y + player1.getSize().y/2)<< "\n";
+        std::cout << ball.getPosition().x << " = x, " << ball.getPosition().y << " = y \n";
+        //std:: cout << (bottomBound.getPosition().y - bottomBound.getSize().y/2)-(player1.getPosition().y + player1.getSize().y/2)<< "\n";
         acceleration.y -= gravity; 
         velocity += acceleration * dt;
         velocity *= fr;
@@ -126,12 +155,59 @@ int main()
             player1.setPosition({player1.getPosition().x ,bottomBound.getPosition().y - bottomBound.getSize().y/2 - player1.getSize().y/2 +1 });
         }
         
+        
+        if(ball.getGlobalBounds().findIntersection(player1.getGlobalBounds())){
+            ballVelocity = {0.0f,0.0f};
+            if(player1.getPosition().x < ball.getPosition().x){
+                //ballAcceleration.x = ACC*2;
+                ballVelocity.x = .5;
+            }
+            else{
+                //ballAcceleration.x = -ACC*2;
+                ballVelocity.x = -.5;
+            }
+            if(player1.getPosition().y > ball.getPosition().y){
+                //ballAcceleration.y = -ACC*2;
+                ballVelocity.y = -.5;
+            }
+            else{
+                //ballAcceleration.y = ACC*2;
+                ballVelocity.y = .5;
+            }
+        }
+        
+        if(bottomBound.getGlobalBounds().findIntersection(ball.getGlobalBounds())){
+            ballAcceleration.y = -.005;
+            ballVelocity.y *= -1;
+        }
+        if(topBound.getGlobalBounds().findIntersection(ball.getGlobalBounds())){
+            ballAcceleration.y = .005;
+            ballVelocity.y *= -1;
+        }
+        if(leftBound.getGlobalBounds().findIntersection(ball.getGlobalBounds())){
+            ballAcceleration.x = .005;
+            ballVelocity.x *= -1;
+        }
+        if(rightBound.getGlobalBounds().findIntersection(ball.getGlobalBounds())){
+            ballAcceleration.x = -.005;
+            ballVelocity.x *= -1;
+        }
+        ballAcceleration.y += .0001;
+        ballVelocity += ballAcceleration * dt;
+        std::cout << "ball vellocity " << ballVelocity.x << " " << ballVelocity.y << "\n";
+        if(dt < 20)ball.move(ballVelocity * dt);
+
+
 
         window.clear();
         window.draw(bound);
         window.draw(bottomBound);
+        window.draw(topBound);
+        window.draw(leftBound);
+        window.draw(rightBound);
         window.draw(player2);
         window.draw(player1);
+        window.draw(ball);
         window.display();
     }
 
